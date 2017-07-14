c33e7e244ddf83bfc33590811df702ae0b735149
PORTMAN <longton1@hotmail.com>
2017-07-14 14:43:56 +0000

    2 player with ai playing 'o'

--- src/Board.js
+++ src/Board.js
@@ -13,83 +13,89 @@ class Board extends Component {
     constructor(){
         super();
         this.state = {
-            grid:["","","O",
-                  "","","",
-                  "X","","",], 
-            winner: null,
-            minPlayer : 'O',
-            maxPlayer : 'X'
+            grid:[" "," "," ",
+                  " "," "," ",
+                  " "," "," ",], 
+            minPlayer : 'o',
+            maxPlayer : 'x',
+            gameOver : false
         }
 
 
        this.gameLoop = this.gameLoop.bind(this);
-       this.checkWinner = this.checkWinner.bind(this);
-       this.isMoveValid = this.isMoveValid.bind(this);
-       this.draw = this.draw.bind(this);
-       this.reset = this.reset.bind(this);
+       this.winner = this.winner.bind(this);
+       this.validMove = this.validMove.bind(this);
+       this.tie = this.tie.bind(this);
        this.copyBoard = this.copyBoard.bind(this);
        this.minScore = this.minScore.bind(this);
        this.maxScore = this.maxScore.bind(this);
-       this.findAIMove = this.findAIMove.bind(this);
+       this.findAiMove = this.findAiMove.bind(this);
+       this.reset = this.reset.bind(this);
     }
 
-    
-
 gameLoop(event){
- let id = event.target.id;
- let player = 'X'
+
+
+    
+    if(this.state.gameOver === false){
+
+         let id = event.target.id;
+         let player = 'x'
    
 
- if(this.state.winner === null){
+ 
       // *************HUMAN PLAYER LOOP******************
-     let currentGameBoard = this.isMoveValid(this.state.grid ,id, player);
+     let currentGameBoard = this.validMove( id, player ,this.state.grid);
     
-        if(this.checkWinner(currentGameBoard, player)){
+        if(this.winner(currentGameBoard, player)){
            
             this.setState({
                 grid: currentGameBoard,
-                winner : player
+                gameOver : true
             });
             return;
         }
-        if (this.draw(currentGameBoard)){
+        if (this.tie(currentGameBoard)){
            
             this.setState({
                 grid: currentGameBoard,
-                winner : 'Draw'
+                gameOver : true
             });
             return;
         }
         
-    //***********AI PLAYER*******************/
+     //***********AI PLAYER*******************/
+     player = 'o';
+     currentGameBoard = this.validMove( this.findAiMove(currentGameBoard), player , currentGameBoard);
 
-    player = 'O';
-    currentGameBoard = this.isMoveValid(currentGameBoard, this.findAIMove(currentGameBoard), player);
+        if(this.winner(currentGameBoard, player)){
+            this.setState({
+                    grid: currentGameBoard,
+                    gameOver : true
+                });
+                return;
+        }
+        if(this.tie(currentGameBoard)){
+            this.setState({
+                    grid: currentGameBoard,
+                    gameOver : true
+                });
+                return;
+        }
 
-    if(this.checkWinner(currentGameBoard, player)){
-         this.setState({
-                grid: currentGameBoard,
-                winner : player
-            });
-            return;
+        this.setState({
+            grid: currentGameBoard,
+
+        });
     }
-    if(this.draw(currentGameBoard)){
-         this.setState({
-                grid: currentGameBoard,
-                winner : 'Draw'
-            });
-            return;
-    }
+}
 
-    this.setState({
-        grid: currentGameBoard,
 
-    });
- }
+ 
   
        
 
-}
+
 
 
 
@@ -98,23 +104,20 @@ gameLoop(event){
 // IF BLANK , COPIES BOARD AND PUTS PLAYER IN NEW POSTION
 // RETURNS NEW BOARD
 // IF NOT BLANK , THEN JUST EXITS FUNCTION
- isMoveValid(board, position, player){
-    let newBoard = this.copyBoard(board);
-    if(board[position] === ""){
-        
-        newBoard[position] = player;
-        return newBoard;
-        }
-    else{
-        return null;
-        }
-    }
+validMove(move, player, board){
+  var newBoard = this.copyBoard(board);
+  if(newBoard[move] === " "){
+    newBoard[move] = player;
+    return newBoard;
+  } else
+    return null;
+}
 
 // CHECK FOR WINNER.. TAKES A BOARD AND PLAYER 
 // FOR LOOP THAT TAKES EACH ARRAY IN WINS ARRAY, THEN USE EACH NUMBER A POSITION TO CHECK ON OUR BOARD
 // IF PLAYER SYMBOL MATCHES THEN RETURN TRUE IF (WINNER)
-checkWinner(board, player){
-    const wins = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[2,4,6],[0,4,8]];
+winner(board, player){
+  const wins = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[2,4,6],[0,4,8]];
 
     for(let i = 0; i < wins.length; i++){
         let win = wins[i];
@@ -123,127 +126,99 @@ checkWinner(board, player){
            {
             return true;
         }
-        
+      
     }
-   
 }
 
-// CHECK FOR DRAW FUNCTION , TAKES A COUNTER OF ALL MOVES MADE SO FAR, BOARD , PLAYER
-// IF COUNTER EQUALLS 8 AND CHECKWNNER FUNCTION RETURNS UNDEFINED THEN RETURN TRUE (DRAW)
-draw(board){
-    let counter = 0 ;
+tie(board) {
+  var moves = board.join('').replace(/ /g, ''); 
 
-    for(let i = 0 ; i < board.length; i++){
-        if(board[i] === ''){
-            counter++;
-        }
-
-        if(counter === 9){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
-
-}
-// RESET STATE TO DEFAULT
-reset(){
-    this.setState(  {
-            grid:["","","","","","","","","",], 
-            movesCounter : 0,
-            winner : null
-        });
+    
+  if (moves.length === 9) {
+    return true;
+  }
+  return false;
 }
 
-copyBoard(board){
-   return board.slice(0);  
+copyBoard(board) {
+  //This returns a new copy of the Board and ensures that you're only
+  //manipulating the copies and not the primary board.
+  return board.slice(0);
 }
 
-
-findAIMove(board){
-        let bestMoveScore = 100;
-        let move = null;
-
-        if(this.checkWinner(board , 'X') || this.checkWinner(board, 'O') || this.draw(board)){
+findAiMove(board) {
+    var bestMoveScore = 100;
+    let move = null;
+     //Test Every Possible Move if the game is not already over.
+        if(this.winner(board, 'x') || this.winner(board, 'o') || this.tie(board)) {
             return null;
         }
-
-        for(let i = 0; i < board.length; i++){
-            // newBoard will equal the board returned from valid move, which makes the move..
-            let newBoard = this.isMoveValid(board, i, this.state.minPlayer);
-            if(newBoard){
-                let moveScore = this.minScore(newBoard);
-                if(moveScore < bestMoveScore){
+            for(var i = 0; i < board.length; i++){
+                let newBoard = this.validMove(i, this.state.minPlayer, board);
+                //If validMove returned a valid game board
+                if(newBoard) {
+                var moveScore = this.maxScore(newBoard);
+                if (moveScore < bestMoveScore) {
                     bestMoveScore = moveScore;
                     move = i;
                 }
+                }
             }
-
-        }
-
-       return move;
+            return move;
 }
 
-minScore(board){
-        if(this.checkWinner(board, 'X')){
-            return 10;
-        }
-         if(this.checkWinner(board, 'O')){
-                return -10;
-        }
-         if(this.draw(board)){
-            return 0;
-        }
-        else{
-           let bestMoveValue = 100;
-           let move = 0;
-            for(let i =0; i < board.length; i++){
-                let newBoard = this.isMoveValid(board, i,this.state.minPlayer);
-                if(newBoard){
-                    let predictedMoveValue = this.maxScore(newBoard);
-                    if(predictedMoveValue < bestMoveValue){
-                        bestMoveValue = predictedMoveValue;
-                        move = i;
-                    }
+minScore(board) {
+  if (this.winner(board, 'x')) {
+    return 10;
+  } else if (this.winner(board, 'o')) {
+    return -10;
+  } else if (this.tie(board)) {
+    return 0;
+  } else {
+        var bestMoveValue = 100;
+            for (var i = 0; i < board.length; i++) {
+            var newBoard = this.validMove(i, this.state.minPlayer, board);
+            if (newBoard) {
+                var predictedMoveValue = this.maxScore(newBoard);
+                if (predictedMoveValue < bestMoveValue) {
+                bestMoveValue = predictedMoveValue;
                 }
             }
-            //console.log("Best Move Value(minScore):", bestMoveValue);
-            return bestMoveValue;
-        }
-    }
+            }
+        return bestMoveValue;
+  }
+}
 
-maxScore(board){
-        if(this.checkWinner(board, 'X')){
-            return 10;
-        }
-         if(this.checkWinner(board, 'O')){
-                return -10;
-        }
-         if(this.draw(board)){
-            return 0;
-        }
-        else{
-           let bestMoveValue = -100;
-           let move= 0;
-            for(let i =0; i < board.length; i++){
-                let newBoard = this.isMoveValid(board, i,this.state.maxPlayer);
-                if(newBoard){
-                    let predictedMoveValue = this.minScore(newBoard);
-                    if(predictedMoveValue > bestMoveValue){
+maxScore(board) {
+   if(this.winner(board, 'x')) {
+    return 10;
+  } else if(this.winner(board, 'o')) {
+    return -10;
+  } else if(this.tie(board)) {
+    return 0;
+  } else {
+ var bestMoveValue = -100;
+        for (var i = 0; i < board.length; i++) {
+            var newBoard = this.validMove(i, this.state.maxPlayer, board);
+                if (newBoard) {
+                    var predictedMoveValue = this.minScore(newBoard);
+                    if (predictedMoveValue > bestMoveValue) {
                         bestMoveValue = predictedMoveValue;
-                        move = i;
                     }
                 }
-            }
-            //console.log("Best Move Value(maxScore):", bestMoveValue);
-            return bestMoveValue;
         }
-    }
+        return bestMoveValue;
+  }
+}
 
-
-
-
+reset(){
+    this.setState({
+         grid:[" "," "," ",
+                  " "," "," ",
+                  " "," "," ",], 
+            gameOver : false
+    });
+}
 
   render() {
 
@@ -253,6 +228,7 @@ maxScore(board){
             
     )
 });
+
 
 
 
